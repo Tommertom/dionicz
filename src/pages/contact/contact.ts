53da1f5eca35a821ef8c6035c3500285ba8c9b01
Tommertom <tom.gruintjes@gmail.com>
2017-10-22 15:59:51 +0200

    Draggabilly

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import * as Packery from 'packery';
+import * as Draggabilly from 'draggabilly';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +10,50 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  @ViewChild('gridPackery') gridPackery;
+
+  draggies = [];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+
+  ionViewDidEnter() {
+
+    setTimeout(() => {
+
+      let elem = document.querySelector('.grid');
+
+      let pckry = new Packery(this.gridPackery.nativeElement, {
+        itemSelector: ".grid-item",
+        gutter: 10,
+        columnWidth: 60
+      });
+
+      pckry.getItemElements().forEach(item => {
+        let draggie = new Draggabilly(item, {grid: [ 20, 20 ]});
+        pckry.bindDraggabillyEvents(draggie);
+        this.draggies.push(draggie);
+
+        draggie.enable();
+        draggie.bindHandles();
+        //draggie.unbindHandles();
+
+        //draggie.disable();
+        //draggie.unbindHandles();
+      })
+
+      /*
+           for (let i = 0; i < this.draggies.length; i++)
+            {
+                this.draggies[i].enable();
+               this.draggies[i].bindHandles();
+            }
+      */
+
+
+    }, 100);
   }
 
 }
