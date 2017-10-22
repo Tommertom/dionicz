dbb46fadd0b47ac3ca802b905eb2c1286deae82b
Tommertom <tom.gruintjes@gmail.com>
2017-10-22 16:11:12 +0200

    Draggabilly

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -13,46 +13,37 @@ export class ContactPage {
   @ViewChild('gridPackery') gridPackery;
 
   draggies = [];
+  pckry:any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
-
+  imgLoaded() {
+    console.log('LOADED image');
+    if (this.pckry) this.pckry.layout();
+  }
+ 
   ionViewDidEnter() {
 
     setTimeout(() => {
 
       let elem = document.querySelector('.grid');
 
-      let pckry = new Packery(this.gridPackery.nativeElement, {
+      this.pckry = new Packery(this.gridPackery.nativeElement, {
         itemSelector: ".grid-item",
         gutter: 10,
         columnWidth: 60
       });
 
-      pckry.getItemElements().forEach(item => {
+      this.pckry.getItemElements().forEach(item => {
         let draggie = new Draggabilly(item, {grid: [ 20, 20 ]});
-        pckry.bindDraggabillyEvents(draggie);
+        this.pckry.bindDraggabillyEvents(draggie);
         this.draggies.push(draggie);
 
         draggie.enable();
         draggie.bindHandles();
-        //draggie.unbindHandles();
-
-        //draggie.disable();
-        //draggie.unbindHandles();
       })
-
-      /*
-           for (let i = 0; i < this.draggies.length; i++)
-            {
-                this.draggies[i].enable();
-               this.draggies[i].bindHandles();
-            }
-      */
-
-
     }, 100);
   }
 
